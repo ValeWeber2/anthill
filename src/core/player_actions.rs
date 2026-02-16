b8398688163c04c765c9378271dbbc18c836e0b7
Weber, Valentin <dinf.vale@gmail.com>
2026-02-16 13:05:11 +0000

    Fixed index out of bounds occurring when moving against the bottom and right world borders.

--- src/core/player_actions.rs
+++ src/core/player_actions.rs
@@ -66,65 +66,73 @@ impl GameState {
     }
 
     pub fn resolve_player_action(&mut self, input: PlayerInput) {
-        let intended_action = self.interpret_player_input(input);
+        if let Some(intended_action) = self.interpret_player_input(input) {
+            let action_result: GameResult = match intended_action {
+                ActionKind::Wait => Ok(GameOutcome::Success),
+                ActionKind::Move(Direction::Up) => self.move_player_character(0, -1),
+                ActionKind::Move(Direction::Right) => self.move_player_character(1, 0),
+                ActionKind::Move(Direction::Down) => self.move_player_character(0, 1),
+                ActionKind::Move(Direction::Left) => self.move_player_character(-1, 0),
+                ActionKind::Attack(npc_id) => self.player_attack_npc(npc_id),
+                ActionKind::PickUpItem(entity_id) => self.pick_up_item(entity_id),
+                ActionKind::DropItem(item_id) => self.drop_item(item_id),
+                ActionKind::UseItem(item_id) => self.use_item(item_id),
+                ActionKind::UnequipWeapon => self.unequip_weapon(),
+                ActionKind::UnequipArmor => self.unequip_armor(),
+                ActionKind::TileInteraction(point) => self.tile_interaction(point),
+                ActionKind::RangedAttack(npc_id) => self.player_ranged_attack_npc(npc_id),
+            };
 
-        let action_result: GameResult = match intended_action {
-            ActionKind::Wait => Ok(GameOutcome::Success),
-            ActionKind::Move(Direction::Up) => self.move_player_character(0, -1),
-            ActionKind::Move(Direction::Right) => self.move_player_character(1, 0),
-            ActionKind::Move(Direction::Down) => self.move_player_character(0, 1),
-            ActionKind::Move(Direction::Left) => self.move_player_character(-1, 0),
-            ActionKind::Attack(npc_id) => self.player_attack_npc(npc_id),
-            ActionKind::PickUpItem(entity_id) => self.pick_up_item(entity_id),
-            ActionKind::DropItem(item_id) => self.drop_item(item_id),
-            ActionKind::UseItem(item_id) => self.use_item(item_id),
-            ActionKind::UnequipWeapon => self.unequip_weapon(),
-            ActionKind::UnequipArmor => self.unequip_armor(),
-            ActionKind::TileInteraction(point) => self.tile_interaction(point),
-            ActionKind::RangedAttack(npc_id) => self.player_ranged_attack_npc(npc_id),
-        };
-
-        match action_result {
-            Ok(GameOutcome::Success) => self.next_round(),
-            Ok(GameOutcome::Fail(reason)) => {
-                // Log for user only if message is defined for user
-                if let Some(log_data) = reason.notify_user() {
-                    self.log.info(log_data);
+            match action_result {
+                Ok(GameOutcome::Success) => self.next_round(),
+                Ok(GameOutcome::Fail(reason)) => {
+                    // Log for user only if message is defined for user
+                    if let Some(log_data) = reason.notify_user() {
+                        self.log.info(log_data);
+                    }
                 }
-            }
-            Err(error) => {
-                // Log for Debugging
-                self.log.debug_warn(error.to_string());
+                Err(error) => {
+                    // Log for Debugging
+                    self.log.debug_warn(error.to_string());
+                }
             }
         }
     }
 
-    pub fn interpret_player_input(&mut self, input: PlayerInput) -> ActionKind {
+    pub fn interpret_player_input(&mut self, input: PlayerInput) -> Option<ActionKind> {
         match input {
-            PlayerInput::Wait => ActionKind::Wait,
+            PlayerInput::Wait => Some(ActionKind::Wait),
             PlayerInput::Direction(direction) => {
                 let target_point: Point = self.player.character.pos().get_adjacent(direction);
-                let target_tile = self.current_world().get_tile(target_point);
+
+                // Fallback if point is out of bounds
+                if !self
+                    .current_world()
+                    .is_in_bounds(target_point.x as isize, target_point.y as isize)
+                {
+                    return None;
+                }
 
                 if let Some(entity_id) = self.current_level().get_npc_at(target_point) {
-                    return ActionKind::Attack(entity_id);
+                    return Some(ActionKind::Attack(entity_id));
                 }
 
                 if let Some(entity_id) = self.current_level().get_item_sprite_at(target_point) {
-                    return ActionKind::PickUpItem(entity_id);
+                    return Some(ActionKind::PickUpItem(entity_id));
                 }
 
+                let target_tile = self.current_world().get_tile(target_point);
                 if target_tile.tile_type.is_interactable() {
-                    return ActionKind::TileInteraction(target_point);
+                    return Some(ActionKind::TileInteraction(target_point));
                 }
 
-                ActionKind::Move(direction)
+                Some(ActionKind::Move(direction))
             }
-            PlayerInput::UseItem(item_id) => ActionKind::UseItem(item_id),
-            PlayerInput::DropItem(item_id) => ActionKind::DropItem(item_id),
-            PlayerInput::UnequipWeapon => ActionKind::UnequipWeapon,
-            PlayerInput::UnequipArmor => ActionKind::UnequipArmor,
-            PlayerInput::RangedAttack(entity_id) => ActionKind::RangedAttack(entity_id),
+            PlayerInput::UseItem(item_id) => Some(ActionKind::UseItem(item_id)),
+            PlayerInput::DropItem(item_id) => Some(ActionKind::DropItem(item_id)),
+            PlayerInput::UnequipWeapon => Some(ActionKind::UnequipWeapon),
+            PlayerInput::UnequipArmor => Some(ActionKind::UnequipArmor),
+            PlayerInput::RangedAttack(entity_id) => Some(ActionKind::RangedAttack(entity_id)),
         }
     }
 
